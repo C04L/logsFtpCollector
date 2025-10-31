6a96ebba2af17892f2b24f70b06eb41cffbf60e6
Coal <coal@coal.coal>
2025-10-31 08:50:03 +0700

    init

--- src/main/java/org/coal/FTPConnection.java
+++ src/main/java/org/coal/FTPConnection.java
@@ -8,9 +8,8 @@ import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 
 import java.io.File;
 import java.io.FileOutputStream;
-import java.io.IOException;
+
 import java.io.OutputStream;
-import java.net.SocketException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
